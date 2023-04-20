0eaf1aa7b91706125946b49265347a945b2eea04
WuDongyang <wudongyang@gmail.com>
2023-04-20 12:46:47 +0800

    修改后台域名

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -296,7 +296,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "http://suc.tvos.io/sub?" }],
         remoteConfig: [
           {
             label: "universal",
